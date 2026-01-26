9022c7ce9ee04fcaa0ee55840e40bdd1a6193135
Riley Hales PhD <39097632+rileyhales@users.noreply.github.com>
2026-01-26 11:33:22 -0700

    prepare quarter degree grace

--- data_preprocessors/prepare_grace.py
+++ data_preprocessors/prepare_grace.py
@@ -1,21 +1,26 @@
 import xarray as xr
 from zarr.codecs import ZstdCodec
 
-ds = xr.open_dataset('./GRC_gw.nc')
-ds['lwe_thickness_anomaly'] = ds['lwe_thickness'] - ds['lwe_thickness'].mean(dim='time')
-ds.to_netcdf('/Users/rchales/data/grace/GRC_gw_anomaly.nc')
+ds = xr.open_dataset('./GRC_025gw.nc')
+ds['lwe_thickness_anomaly'] = (ds['lwe_thickness'] - ds['lwe_thickness'].mean(dim='time')).round(3)
+ds['lwe_thickness'] = ds['lwe_thickness'].round(3)
+ds['uncertainty'] = ds['uncertainty'].round(3)
+ds.to_netcdf('./GRC_025gw_anomaly.nc')
 (
     ds
-    # for 1 degree tiles
     .chunk({
-        'time': 199,
-        'lat': 2,
-        'lon': 2,
+        'time': ds['time'].shape[0],
+        'lat': 16,
+        'lon': 16,
     })
     .to_zarr(
-        './GRC_gw_anomaly.zarr',
+        './grace025gwanomaly.zarr3',
         mode='w',
         zarr_version=3,
-        encoding={'lwe_thickness_anomaly': {'compressors': ZstdCodec(level=5)}}
+        encoding={
+            'lwe_thickness_anomaly': {'compressors': ZstdCodec(level=5)},
+            'lwe_thickness': {'compressors': ZstdCodec(level=5)},
+            'uncertainty': {'compressors': ZstdCodec(level=5)},
+        }
     )
 )
